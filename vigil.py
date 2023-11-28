136a4c866b8cc0f518d3d57fc459a4db4583ef28
Mehul Pathak <pathakkmehul@gmail.com>
2023-11-28 19:48:29 +0530

    updated code

--- vigil.py
+++ vigil.py
@@ -1,31 +1,18 @@
-#Vigil - Under Construction
 import requests
-from urllib.parse import urlparse
 
-def get_redirected_url(url):
-    redirects = []
-    while True:
-        response = requests.head(url, allow_redirects=False)
-        if response.status_code in (301, 302, 303, 307, 308):
-            next_url = response.headers.get('Location')
-            if not next_url:
-                break
-            redirects.append(url)
-            url = next_url
-        else:
-            break
-    return redirects, url
+def checkRedirects(url):
+    response = requests.get(url)
+    if response.history:
+        counter=1
+        for redirect in (response.history):
+            print(f"[{counter}] URL: {redirect.url}")
+            print(f" ↓  Status Code: {redirect.status_code}")
+            counter+=1
+        print(f"Final URL: {response.url}")
+        print(f"Final Status Code: {response.status_code}")
+    else:
+        print(f"{url} does not redirect.")
 
-def main():
-    input_url = input("Enter the URL: ")
-    redirects, final_destination = get_redirected_url(input_url)
-    
-    print("Redirect Hops:")
-    for i, redirect in enumerate(redirects):
-        print(f"{i + 1}: {redirect}")
-    
-    print("Final Destination:")
-    print(final_destination)
-
-if __name__ == "__main__":
-    main()
+n = input("Enter the link: ")
+print()
+checkRedirects(n)
